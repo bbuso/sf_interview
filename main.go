8a93c0535ac583ca149a26d85d351511c02f0bcf
B Buso <bubu@MacBook-Pro.localdomain>
2022-06-06 00:20:38 -0400

    working functions

--- main.go
+++ main.go
@@ -6,9 +6,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 	"sync"
 )
@@ -20,13 +18,14 @@ type BufferedResponse interface {
 func readCSVFiles(paths []string) {
 	var wg sync.WaitGroup
 	for _, filepath := range paths {
+		wg.Add(1)
 		go fmt.Println(readCSVFile(filepath, &wg))
 	}
 	wg.Wait()
 }
 
 func readCSVFile(filePath string, wg *sync.WaitGroup) bool {
-	wg.Add(1)
+
 	defer wg.Done()
 	f, err := os.Open(filePath)
 	if err != nil {
@@ -54,7 +53,7 @@ func readCSVFile(filePath string, wg *sync.WaitGroup) bool {
 
 }
 
-func ByteCompressor(reader io.ReadCloser) []byte {
+func ByteCompressor(reader io.ReadCloser) BufferedResponse {
 	read_bytes, err := io.ReadAll(reader)
 	if err != nil {
 		log.Fatal(err)
@@ -64,36 +63,11 @@ func ByteCompressor(reader io.ReadCloser) []byte {
 	writer := gzip.NewWriter(&zipped)
 	writer.Write(read_bytes)
 	writer.Close()
-	// test := bytes.NewReader(zipped.Bytes())
-	// fmt.Println(reflect.TypeOf(test))
-	return zipped.Bytes()
+	r := bytes.NewReader(zipped.Bytes())
+
+	return r
 }
 
 func main() {
-
-	client := &http.Client{}
-	req, err := http.NewRequest(http.MethodGet, "https://www.google.com/", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	response := ByteCompressor(resp.Body)
-	test_err := os.WriteFile("./test.html.gz", response, 0644)
-	if test_err != nil {
-		log.Fatal(err)
-	}
-
-	// readCSVFile("./testdata/TestProcessEligibleChannel2_0_TestProcessEligibleChannel2_0_CODES.csv")
-	files, _ := ioutil.ReadDir("./testdata")
-	paths := []string{}
-	for _, file := range files {
-		filepath := "./testdata/" + file.Name()
-		paths = append(paths, filepath)
-	}
-	readCSVFiles(paths)
 
 }
